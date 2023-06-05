f1fdb1ef5e9508c0188a0e5389db7916a1cbf96b
nnerminbasaran <n.nerminbasaran@gmail.com>
2023-06-05 14:06:49 +0300

    framework uploaded

---
+++ src/test/java/com/myapp/pages/PearlyMarketRegisterPage.java
@@ -0,0 +1,27 @@
+package com.myapp.pages;
+
+import com.myapp.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PearlyMarketRegisterPage {
+    public PearlyMarketRegisterPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//div//a[.='Become a Vendor']")
+    public WebElement becomeAVendorLink;
+
+    @FindBy(id = "user_email")
+    public WebElement email;
+    @FindBy(xpath = "//input[@placeholder='Verification Code']")
+    public WebElement verificationCode;
+    @FindBy(id = "password")
+    public WebElement password;
+    @FindBy(id = "confirm_pwd")
+    public WebElement confirmPassword;
+    @FindBy(id = "wcfm_membership_register_button")
+    public WebElement registerButton;
+
+}
