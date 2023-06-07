534b74641bc9a4a1f0889bbf1d2919241c65e047
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2023-06-07 14:12:05 +0200

    Framework update June 7

--- src/test/java/com/myapp/tests/US_14/TC_01.java
+++ src/test/java/com/myapp/tests/US_14/TC_01.java
@@ -1,6 +1,6 @@
 package com.myapp.tests.US_14;
 
-import com.myapp.pages.PMAddProductPage;
+import com.myapp.pages.PearlyMarketAddProductPage;
 import com.myapp.pages.PearlyMarketHomePage;
 import com.myapp.utilities.ReusableMethods;
 import org.testng.Assert;
@@ -10,13 +10,13 @@ public class TC_01 {
 
     ReusableMethods reMethods = new ReusableMethods();
     PearlyMarketHomePage pmHomePage = new PearlyMarketHomePage();
-    PMAddProductPage pmAddProductPage = new PMAddProductPage();
+    PearlyMarketAddProductPage pmAddProductPage = new PearlyMarketAddProductPage();
 
     @Test
     public void addNewProductMenuTest(){
         reMethods.signIn();
         reMethods.scrollPageEndActions();
-        reMethods.waitForVisibility(pmHomePage.myAccountButton, 11);
+        // reMethods.waitForVisibility(pmHomePage.myAccountButton, 11);
         pmHomePage.myAccountButton.click();
         pmHomePage.storeManagerButton.click();
         pmHomePage.myStoreProductsElement.click();
