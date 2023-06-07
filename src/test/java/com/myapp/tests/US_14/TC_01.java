c1843e8c33222fede70119f47f90b31b05516238
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2023-06-07 12:38:49 +0200

    Update TC_01.java
    US14 TC01 updated

--- src/test/java/com/myapp/tests/US_14/TC_01.java
+++ src/test/java/com/myapp/tests/US_14/TC_01.java
@@ -1,17 +1,34 @@
 package com.myapp.tests.US_14;
 
+import com.myapp.pages.PMAddProductPage;
 import com.myapp.pages.PearlyMarketHomePage;
 import com.myapp.utilities.ReusableMethods;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TC_01 {
 
     ReusableMethods reMethods = new ReusableMethods();
     PearlyMarketHomePage pmHomePage = new PearlyMarketHomePage();
+    PMAddProductPage pmAddProductPage = new PMAddProductPage();
 
     @Test
     public void addNewProductMenuTest(){
         reMethods.signIn();
+        reMethods.scrollPageEndActions();
+        reMethods.waitForVisibility(pmHomePage.myAccountButton, 11);
+        pmHomePage.myAccountButton.click();
+        pmHomePage.storeManagerButton.click();
+        pmHomePage.myStoreProductsElement.click();
+        reMethods.hoverOverOnElementActions(pmHomePage.myStoreProductsElement);
+        pmHomePage.productsAddnewElement.click();
+
+        pmAddProductPage.dropdownProductTypeElement.click();
+        Assert.assertTrue(pmAddProductPage.dropdownSimpleProductElement.isDisplayed());
+        Assert.assertTrue(pmAddProductPage.dropdownVariableProductElement.isDisplayed());
+        Assert.assertTrue(pmAddProductPage.dropdownGroupedProductElement.isDisplayed());
+        Assert.assertTrue(pmAddProductPage.dropdownExternalProductElement.isDisplayed());
+
     }
 
 }
