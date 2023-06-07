73a31f81981277785062978a7b5fa7983ac652ce
Abraham Can <95496005+ibrkorkut07@users.noreply.github.com>
2023-06-07 13:42:31 +0200

    Update PearlyMarketHomePage.java

--- src/test/java/com/myapp/pages/PearlyMarketHomePage.java
+++ src/test/java/com/myapp/pages/PearlyMarketHomePage.java
@@ -1,34 +1,54 @@
-package com.myapp.tests.US_14;
+package com.myapp.pages;
 
-import com.myapp.pages.PearlyMarketAddProductPage;
-import com.myapp.pages.PearlyMarketHomePage;
-import com.myapp.utilities.ReusableMethods;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import com.myapp.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class TC_01 {
 
-    ReusableMethods reMethods = new ReusableMethods();
-    PearlyMarketHomePage pmHomePage = new PearlyMarketHomePage();
-    PearlyMarketAddProductPage pmAddProductPage = new PearlyMarketAddProductPage();
-
-    @Test
-    public void addNewProductMenuTest(){
-        reMethods.signIn();
-        reMethods.scrollPageEndActions();
-        reMethods.waitForVisibility(pmHomePage.myAccountButton, 11);
-        pmHomePage.myAccountButton.click();
-        pmHomePage.storeManagerButton.click();
-        pmHomePage.myStoreProductsElement.click();
-        reMethods.hoverOverOnElementActions(pmHomePage.myStoreProductsElement);
-        pmHomePage.productsAddnewElement.click();
-
-        pmAddProductPage.dropdownProductTypeElement.click();
-        Assert.assertTrue(pmAddProductPage.dropdownSimpleProductElement.isDisplayed());
-        Assert.assertTrue(pmAddProductPage.dropdownVariableProductElement.isDisplayed());
-        Assert.assertTrue(pmAddProductPage.dropdownGroupedProductElement.isDisplayed());
-        Assert.assertTrue(pmAddProductPage.dropdownExternalProductElement.isDisplayed());
+public class PearlyMarketHomePage {
+    public PearlyMarketHomePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
 
     }
+
+    /*
+       After click to SignIn/Register then ALERT appears.
+       And In Alert we have SIGN UP and SIGN IN parts
+       */
+    //<<<<<<< HEAD
+    @FindBy(xpath = "//span[text()='Sign In']")
+    public WebElement homepageSignInButton;
+    @FindBy(xpath = "//span[text()='Register']")
+    public WebElement homepageRegisterButton;
+    //=======
+
+    // ************** SIGN IN / SIGN UP **********
+    @FindBy(xpath = "//input[@id='username']")// for SIGN IN & SIGN UP
+    public WebElement usernameBox;
+    @FindBy(xpath = "//input[@id='password']")// for SIGN IN
+    public WebElement passwordBox;
+    @FindBy(xpath = "//input[@id='reg_password']")// for SIGN UP
+    public WebElement signUpPasswordBox;
+    @FindBy(xpath = "//input[@id='reg_email']")// for SIGN UP
+    public WebElement emailBox;
+    @FindBy(id= "rememberme")// for SIGN IN
+    public WebElement rememberMe;
+    @FindBy(id= "'register-policy") // for SIGN UP
+    public WebElement registerPolicy; // To select "I agree to the privacy policy"
+    @FindBy(xpath = "//button[@name='login']") // for SIGN IN
+    public WebElement loginButton;
+    @FindBy(xpath = "//button[@name='register']")// for SIGN UP
+    public WebElement signUpButton;
+    // *********************************************
+    @FindBy(xpath = "/html/body/div[2]/footer/div/section[2]/div/div[2]/div/section/div/div[2]/div/div[2]/div/nav/ul/li[4]")
+    public WebElement myAccountButton;
+    @FindBy(xpath = "//a[@href='https://pearlymarket.com/store-manager/']")
+    public WebElement storeManagerButton;
+    @FindBy(xpath = "(//a[@class='wcfm_menu_item '])[3]")
+    public WebElement myStoreProductsElement;
+    @FindBy(xpath = "(//span[@class='text'])[18]")
+    public WebElement productsAddnewElement;
+
 
 }
